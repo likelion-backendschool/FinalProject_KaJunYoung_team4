2ef2d02933ed36e7d744f9dee898675bf74510d8
Jwhyee <kawnsdud@naver.com>
2022-10-17 16:09:51 +0900

    Add #3 : Add home contoller, index page

---
+++ 1Week_Mission/weekOne/src/main/java/com/finalProject/weekOne/web/controller/home/HomeController.java
@@ -0,0 +1,13 @@
+package com.finalProject.weekOne.web.controller.home;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+
+@Controller
+public class HomeController {
+    @GetMapping("/")
+    public String showMain() {
+        return "home/main";
+    }
+}
+
