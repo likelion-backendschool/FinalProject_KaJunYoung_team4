01996bdc8d4adb491cc48bbd1dede88aa7c5e6e4
Jwhyee <kawnsdud@naver.com>
2022-10-17 13:12:34 +0900

    feat #2 : 시큐리티 멤버 구현

---
+++ 1Week_Mission/weekOne/src/main/java/com/finalProject/weekOne/config/security/service/CustomUserDetailsService.java
@@ -0,0 +1,36 @@
+package com.finalProject.weekOne.config.security.service;
+
+import com.finalProject.weekOne.domain.member.AuthMember;
+import com.finalProject.weekOne.domain.member.Member;
+import com.finalProject.weekOne.domain.member.MemberRepository;
+import lombok.RequiredArgsConstructor;
+import org.springframework.security.core.GrantedAuthority;
+import org.springframework.security.core.authority.SimpleGrantedAuthority;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
+import org.springframework.stereotype.Service;
+
+import java.util.ArrayList;
+import java.util.List;
+
+@Service
+@RequiredArgsConstructor
+public class CustomUserDetailsService implements UserDetailsService {
+    private final MemberRepository memberRepository;
+
+    @Override
+    public UserDetails loadUserByUsername(String username) throws UsernameNotFoundException {
+        Member member = memberRepository.findByUsername(username).get();
+
+        List<GrantedAuthority> authorities = new ArrayList<>();
+
+        if (member.getAuthLevel() == 7) {
+            authorities.add(new SimpleGrantedAuthority("ADMIN"));
+        }
+
+        authorities.add(new SimpleGrantedAuthority("MEMBER"));
+
+        return new AuthMember(member, authorities);
+    }
+}
--- 1Week_Mission/weekOne/src/main/java/com/finalProject/weekOne/domain/member/AuthMember.java
+++ 1Week_Mission/weekOne/src/main/java/com/finalProject/weekOne/domain/member/AuthMember.java
@@ -7,28 +7,41 @@ import org.springframework.security.core.userdetails.User;
 
 import java.time.LocalDateTime;
 import java.util.HashSet;
+import java.util.List;
 import java.util.Set;
 
 @Getter
-@JsonIncludeProperties({"id", "createDate", "modifyDate", "username", "nickname", "email", "authorities"})
 public class AuthMember extends User {
-    private final long id;
+    private final Long id;
     private final LocalDateTime createDate;
     private final LocalDateTime modifyDate;
     private final String username;
     private final String nickname;
     private final String email;
-    private final Set<GrantedAuthority> authorities;
 
-    public AuthMember(Member member) {
-        super(member.getUsername(), "", member.getAuthorities());
+    public AuthMember(Member member, List<GrantedAuthority> authorities) {
+        super(member.getUsername(), member.getPassword(), authorities);
+        this.id = member.getId();
+        this.createDate = member.getCreateDate();
+        this.modifyDate = member.getModifyDate();
+        this.username = member.getUsername();
+        this.nickname = member.getNickname();
+        this.email = member.getEmail();
+    }
 
-        id = member.getId();
-        createDate = member.getCreateDate();
-        modifyDate = member.getModifyDate();
-        username = member.getUsername();
-        nickname = member.getNickname();
-        email = member.getEmail();
-        authorities = new HashSet<>(member.getAuthorities());
+    public Member getMember() {
+        return Member
+                .builder()
+                .id(id)
+                .createDate(createDate)
+                .modifyDate(modifyDate)
+                .username(username)
+                .nickname(nickname)
+                .email(email)
+                .build();
+    }
+
+    public String getName() {
+        return getUsername();
     }
 }
