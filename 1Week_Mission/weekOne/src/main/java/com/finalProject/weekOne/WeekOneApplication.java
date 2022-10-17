a0a66339c10c3aa26dcc76892824c22502f4013c
Jwhyee <kawnsdud@naver.com>
2022-10-17 10:22:04 +0900

    add #2 : Project Setting

---
+++ 1Week_Mission/weekOne/src/main/java/com/finalProject/weekOne/WeekOneApplication.java
@@ -0,0 +1,13 @@
+package com.finalProject.weekOne;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WeekOneApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(WeekOneApplication.class, args);
+	}
+
+}
