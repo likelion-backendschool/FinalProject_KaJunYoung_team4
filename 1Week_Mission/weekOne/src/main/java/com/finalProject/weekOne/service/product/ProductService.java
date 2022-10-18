6a833c58409d429f4e7b0cdbccb7b1650437b847
Jwhyee <kawnsdud@naver.com>
2022-10-19 06:50:19 +0900

    Feat #3 : 도서 조회 및 리스트 구현

--- 1Week_Mission/weekOne/src/main/java/com/finalProject/weekOne/service/product/ProductService.java
+++ 1Week_Mission/weekOne/src/main/java/com/finalProject/weekOne/service/product/ProductService.java
@@ -1,6 +1,7 @@
 package com.finalProject.weekOne.service.product;
 
 import com.finalProject.weekOne.domain.member.Member;
+import com.finalProject.weekOne.domain.post.Post;
 import com.finalProject.weekOne.domain.product.Product;
 import com.finalProject.weekOne.domain.product.ProductRepository;
 import com.finalProject.weekOne.web.dto.product.CreateProductDto;
@@ -9,6 +10,7 @@ import lombok.extern.slf4j.Slf4j;
 import org.springframework.stereotype.Service;
 
 import java.time.LocalDateTime;
+import java.util.List;
 
 @Service
 @Slf4j
@@ -17,7 +19,7 @@ public class ProductService {
     private final ProductRepository productRepository;
 
     /**
-     * 새로운 게시물을 저장하는 메소드
+     * 새로운 Product를 저장하는 메소드
      *
      * @param member        현재 로그인된 Member
      * @param createProductDto subject, price, postKeywordId가 포함된 DTO
@@ -34,5 +36,18 @@ public class ProductService {
         productRepository.save(newProduct);
 
         return newProduct;
+    }
+
+    /** id로 Product 객체를 찾는 메소드
+     * @param id 찾으려는 product.id 번호
+     */
+    public Product findByProductId(Long id) {
+        return productRepository.findById(id).orElse(null);
+    }
+
+    /** 모든 Product를 불러오는 메소드
+     */
+    public List<Product> findAllProduct() {
+        return productRepository.findAll();
     }
 }
--- 1Week_Mission/weekOne/src/main/java/com/finalProject/weekOne/web/controller/product/ProductController.java
+++ 1Week_Mission/weekOne/src/main/java/com/finalProject/weekOne/web/controller/product/ProductController.java
@@ -1,10 +1,8 @@
 package com.finalProject.weekOne.web.controller.product;
 
 import com.finalProject.weekOne.domain.member.AuthMember;
-import com.finalProject.weekOne.domain.post.Post;
-import com.finalProject.weekOne.domain.product.Product;
 import com.finalProject.weekOne.service.product.ProductService;
-import com.finalProject.weekOne.web.dto.post.CreatePostDto;
+import com.finalProject.weekOne.domain.product.Product;
 import com.finalProject.weekOne.web.dto.product.CreateProductDto;
 import lombok.RequiredArgsConstructor;
 import org.springframework.security.access.prepost.PreAuthorize;
@@ -13,11 +11,13 @@ import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.validation.BindingResult;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.servlet.mvc.support.RedirectAttributes;
 
 import javax.validation.Valid;
+import java.util.List;
 
 @Controller
 @RequestMapping("/product")
@@ -46,5 +46,21 @@ public class ProductController {
         Product savedProduct = productService.saveProduct(authMember.getMember(), createProductDto);
         redirectAttributes.addAttribute("id", savedProduct.getId());
         return "redirect:/product/{id}";
+    }
+
+    @GetMapping("/list")
+    @PreAuthorize("isAuthenticated()")
+    public String showPostList(Model model) {
+        List<Product> products = productService.findAllProduct();
+        model.addAttribute("products", products);
+        return "product/list";
+    }
+
+    @GetMapping("/{id}")
+    @PreAuthorize("isAuthenticated()")
+    public String showPostDetail(@PathVariable Long id, Model model) {
+        Product currentProduct = productService.findByProductId(id);
+        model.addAttribute("product", currentProduct);
+        return "product/detail";
     }
 }
