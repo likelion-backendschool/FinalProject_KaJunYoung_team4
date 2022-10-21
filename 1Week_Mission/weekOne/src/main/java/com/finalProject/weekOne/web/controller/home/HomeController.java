f617375fa5297e09a7ce76afd089b3e0e2facab1
Jwhyee <kawnsdud@naver.com>
2022-10-21 13:28:28 +0900

    Refactor #4 : Re-login when change member info

--- 1Week_Mission/weekOne/src/main/java/com/finalProject/weekOne/web/controller/home/HomeController.java
+++ 1Week_Mission/weekOne/src/main/java/com/finalProject/weekOne/web/controller/home/HomeController.java
@@ -1,12 +1,24 @@
 package com.finalProject.weekOne.web.controller.home;
 
+import com.finalProject.weekOne.domain.post.Post;
+import com.finalProject.weekOne.service.post.PostService;
+import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 
+import java.util.List;
+
 @Controller
+@RequiredArgsConstructor
 public class HomeController {
+
+    private final PostService postService;
+
     @GetMapping("/")
-    public String showMain() {
+    public String showMain(Model model) {
+        List<Post> posts = postService.findAllPost();
+        model.addAttribute("posts", posts);
         return "home/main";
     }
 }
