7ba384dfda44c1d241503d628ef0f2caf7f479fb
Jwhyee <kawnsdud@naver.com>
2022-10-25 13:46:03 +0900

    Feat #7 : 장바구니 리스트 구현

--- 2Week_Mission/mutbook/src/main/java/com/finalProject/mutbook/web/controller/cartItem/CartItemController.java
+++ 2Week_Mission/mutbook/src/main/java/com/finalProject/mutbook/web/controller/cartItem/CartItemController.java
@@ -1,5 +1,6 @@
 package com.finalProject.mutbook.web.controller.cartItem;
 
+import com.finalProject.mutbook.domain.cartItem.CartItem;
 import com.finalProject.mutbook.domain.member.AuthMember;
 import com.finalProject.mutbook.domain.product.Product;
 import com.finalProject.mutbook.service.cartItem.CartItemService;
@@ -9,11 +10,14 @@ import lombok.extern.slf4j.Slf4j;
 import org.springframework.security.access.prepost.PreAuthorize;
 import org.springframework.security.core.annotation.AuthenticationPrincipal;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.servlet.mvc.support.RedirectAttributes;
+
+import java.util.List;
 
 @Controller
 @Slf4j
@@ -35,5 +39,13 @@ public class CartItemController {
         redirectAttributes.addFlashAttribute("result", "장바구니에 추가했습니다.(%s)".formatted(product.getSubject()));
 
         return "redirect:/product/{productId}";
+    }
+
+    @GetMapping("/list")
+    @PreAuthorize("isAuthenticated()")
+    public String showCartItemList(@AuthenticationPrincipal AuthMember authMember, Model model) {
+        List<CartItem> memberItems = cartItemService.findAllByBuyer(authMember.getMember());
+        model.addAttribute("items", memberItems);
+        return "cart/list";
     }
 }
